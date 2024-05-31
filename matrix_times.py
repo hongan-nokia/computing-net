99c675d4539b979912ebd6b5bbbf7920c7cace55
honggan-nokia <honggang.yuan@nokia-sbell.com>
2024-05-31 13:58:34 +0800

    matrix

---
+++ matrix_times.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@Time: 5/31/2024 1:56 PM
+@Author: Honggang Yuan
+@Email: honggang.yuan@nokia-sbell.com
+Description: 
+"""
+from time import sleep
+
+import numpy as np
+
+
+def matrix_util(dim):
+    X = np.random.randn(dim, dim)
+    Y = np.random.randn(dim, dim)
+    Z = X.dot(Y)
+    print(Z)
+
+
+if __name__ == '__main__':
+    while True:
+        matrix_util(100)
+        sleep(1)
