14e7726672e7c413fcddd0847b36b0d517c438a9
honggan-nokia <honggang.yuan@nokia-sbell.com>
2024-06-11 10:51:32 +0800

    mac

--- cpn_main_mac.py
+++ cpn_main_mac.py
@@ -21,6 +21,7 @@ from nodemodels.cfndemomanager import CfnDemoManager
 from resourcevisiualize.resvisualize import data_visualize
 from testscenes.computingpowerawareaddressroute import ComputingPowerAwareAddressRouteWindow
 from testscenes.scene3 import Scene3
+from testscenes.scene4 import Scene4
 from testscenes.systemsyntheticresutilize import SystemSyntheticResUtilize
 from utils.configparser import DemoConfigParser
 from utils.repeatimer import repeatTimer
@@ -66,14 +67,17 @@ class CpnAppWindow(QtWidgets.QMainWindow):
         self.SSRUWidget.setVisible(False)
         self.scene3 = Scene3(self, cfn_manager)
         self.scene3.setVisible(False)
+        self.scene4 = Scene4(self, cfn_manager)
+        self.scene4.setVisible(False)
 
     def _initDataVisualize(self):
         self.data_visual = data_visualize(parent=self, demo_manager=self.cfn_manager, res_queue_dict=None)
+
         self.data_visual.setVisible(False)
         self.computingNetResMonTimer = QtCore.QTimer(self)
         self.computingNetResMonTimer.setInterval(3000)
         self.computingNetResMonTimer.timeout.connect(self.data_visual.updateNodesInfo)
-        self.computingNetResMonTimer.start()
+        # self.computingNetResMonTimer.start()
         # self.data_mon = repeatTimer(3, self.data_visual.updateNodesInfo, autostart=True)
         # self.data_mon.start()
 
@@ -156,11 +160,22 @@ class CpnAppWindow(QtWidgets.QMainWindow):
         palette.setColor(QPalette.ButtonText, QColor(255, 255, 255))  # 设置按钮上字体的颜色为红色
         self.test3_btn.setPalette(palette)
 
+        self.test4_btn = QPushButton(parent=self)
+        self.test4_btn.setText("测试四")
+        self.test4_btn.setFont(btn_font)
+        self.test4_btn.setGeometry(20, 320, 100, 60)
+        self.test4_btn.setStyleSheet(
+            "border-radius:8px;border-style:outset;border:none;background-color: {}".format(QColor(0, 45, 127).name()))
+        palette = self.test4_btn.palette()
+        palette.setColor(QPalette.ButtonText, QColor(255, 255, 255))  # 设置按钮上字体的颜色为红色
+        self.test4_btn.setPalette(palette)
+
         self.net_compute_aware_btn.show()
         self.main_page_btn.show()
         self.test1_btn.show()
         self.test2_btn.show()
         self.test3_btn.show()
+        self.test4_btn.show()
 
         self.net_compute_aware_btn.clicked.connect(self._resourceAwarenessShow)
         self.main_page_btn.clicked.connect(self._showMainPage)
@@ -168,6 +183,7 @@ class CpnAppWindow(QtWidgets.QMainWindow):
 
         self.test2_btn.clicked.connect(self._showTestScene2)
         self.test3_btn.clicked.connect(self._showTestScene3)
+        self.test4_btn.clicked.connect(self._showTestScene4)
 
     def _resourceAwarenessShow(self):
         print("_resourceAwarenessShow")
@@ -177,9 +193,11 @@ class CpnAppWindow(QtWidgets.QMainWindow):
             self.data_visual.history1.setVisible(False)
             self.data_visual.history2.setVisible(False)
             self.data_visual.history3.setVisible(False)
+            self.computingNetResMonTimer.stop()
         else:
             print("self.data_visual.isVisible() -> False")
             self.data_visual.setVisible(True)
+            self.computingNetResMonTimer.start()
 
     def _showMainPage(self):
         self.CPAARWidget.setVisible(False)
@@ -188,29 +206,53 @@ class CpnAppWindow(QtWidgets.QMainWindow):
         self.scene3.scene31.setVisible(False)
         self.scene3.scene32.setVisible(False)
         self.scene3.scene33.setVisible(False)
+        self.scene4.setVisible(False)
+        self.CPAARWidget.reset()
+        self.SSRUWidget.reset()
+        self.scene3.reset()
+        self.scene3.scene31.reset()
+        self.scene3.scene32.reset()
+        self.scene3.scene33.reset()
+        self.scene4.reset()
         print("Show Main Page")
 
     def _showTestScene1(self):
         self.SSRUWidget.setVisible(False)
         self.CPAARWidget.reset()
-        self.CPAARWidget.user_first_pkg.label.setVisible(True)
+        self.SSRUWidget.reset()
+        self.scene3.reset()
+        self.scene3.scene31.reset()
+        self.scene3.scene32.reset()
+        self.scene3.scene33.reset()
+        self.scene4.reset()
+        # self.CPAARWidget.user_first_pkg.label.setVisible(True)
         self.CPAARWidget.setVisible(True)
         self.scene3.setVisible(False)
         self.scene3.scene31.setVisible(False)
         self.scene3.scene32.setVisible(False)
         self.scene3.scene33.setVisible(False)
+        self.scene4.setVisible(False)
+        self.CPAARWidget.start_timer()
         print("This is TestScene1")
 
     def _showTestScene2(self):
         self.CPAARWidget.setVisible(False)
 
+        self.CPAARWidget.reset()
         self.SSRUWidget.reset()
+        self.scene3.reset()
+        self.scene3.scene31.reset()
+        self.scene3.scene32.reset()
+        self.scene3.scene33.reset()
+        self.scene4.reset()
 
         self.SSRUWidget.setVisible(True)
         self.scene3.setVisible(False)
         self.scene3.scene31.setVisible(False)
         self.scene3.scene32.setVisible(False)
         self.scene3.scene33.setVisible(False)
+        self.scene4.setVisible(False)
+        self.SSRUWidget.start_timer()
         print("This is TestScene2")
 
     def _showTestScene3(self):
@@ -220,8 +262,38 @@ class CpnAppWindow(QtWidgets.QMainWindow):
         self.scene3.scene31.setVisible(False)
         self.scene3.scene32.setVisible(False)
         self.scene3.scene33.setVisible(False)
+        self.scene4.setVisible(False)
+        self.CPAARWidget.reset()
+        self.SSRUWidget.reset()
         self.scene3.reset()
+        self.scene3.scene31.reset()
+        self.scene3.scene32.reset()
+        self.scene3.scene33.reset()
+        self.scene4.reset()
         print("This is TestScene3")
+
+    def _showTestScene4(self):
+        self.SSRUWidget.setVisible(False)
+        self.CPAARWidget.setVisible(False)
+        self.scene3.setVisible(True)
+        self.scene3.scene31.setVisible(False)
+        self.scene3.scene32.setVisible(False)
+        self.scene3.scene33.setVisible(False)
+        self.scene4.setVisible(True)
+        self.CPAARWidget.reset()
+        self.SSRUWidget.reset()
+        self.scene3.reset()
+        self.scene3.scene31.reset()
+        self.scene3.scene32.reset()
+        self.scene3.scene33.reset()
+        self.scene4.reset()
+        sleep(0.2)
+        self.scene4.addrRequest()
+        self.scene4.drawRateAndTime()
+        self.scene4.timer.start()
+        self.scene4.timer_count_lable.start()
+
+        print("This is TestScene4")
 
     def keyPressEvent(self, KEvent):
         k = KEvent.key()
@@ -230,34 +302,35 @@ class CpnAppWindow(QtWidgets.QMainWindow):
                 self.reset()
             elif self.CPAARWidget.isVisible():
                 self.CPAARWidget.reset()
+            elif self.SSRUWidget.isVisible():
+                self.SSRUWidget.reset()
             elif self.scene3.scene31.isVisible():
                 self.scene3.scene31.reset()
             elif self.scene3.scene32.isVisible():
                 self.scene3.scene32.reset()
             elif self.scene3.scene33.isVisible():
                 self.scene3.scene33.reset()
-        elif k == QtCore.Qt.Key_2:
-            self.CPAARWidget.reset()
-            self.CPAARWidget.user_first_pkg.label.setVisible(True)
-            self.CPAARWidget.user_first_pkg.start("sc1_sp1")
         elif k == QtCore.Qt.Key_1:
             print("Pressed Key-1")
-            # self.CPAARWidget.reset()
             self.CPAARWidget.deployAITrainerOnCNode1()
+        elif k == QtCore.Qt.Key_2:
+            self.CPAARWidget.reset()
+            # self.CPAARWidget.user_first_pkg.label.setVisible(True)
+            self.CPAARWidget.user_first_pkg.start("sc1_sp1")
         elif k == QtCore.Qt.Key_Q:
             print("Pressed Key-Q")
             self.scene3.scene31.reset()
-            self.scene3.scene31.service_step1.label.setVisible(True)
+            # self.scene3.scene31.service_step1.label.setVisible(True)
             self.scene3.scene31.service_step1.start("sp1")
         elif k == QtCore.Qt.Key_W:
             self.scene3.scene32.reset()
-            self.scene3.scene32.service_step1.label.setVisible(True)
+            # self.scene3.scene32.service_step1.label.setVisible(True)
             self.scene3.scene32.step1_label1.setVisible(True)
             self.scene3.scene32.step1_label2.setVisible(True)
             self.scene3.scene32.service_step1.start("sp1")
         elif k == QtCore.Qt.Key_E:
             self.scene3.scene33.reset()
-            self.scene3.scene33.service_step1.label.setVisible(True)
+            # self.scene3.scene33.service_step1.label.setVisible(True)
             self.scene3.scene33.service_step1.start("sp1")
         elif k == QtCore.Qt.Key_N:
             self.cfn_manager.send_command("c_node1", "task", "surveillance up")
@@ -282,4 +355,3 @@ if __name__ == '__main__':
     mainWidget = CpnAppWindow(cfn_manager)
     mainWidget.show()
     sys.exit(app.exec())
-
